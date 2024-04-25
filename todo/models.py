79f4a3e0ead1143c10dab315492b9dbb764a6ba5
Galya Fedorenko <f.g.i.mind@gmail.com>
2024-04-25 13:07:17 +0100

    Add basic Task model

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Task(models.Model):
+
+    class TaskPriority(models.IntegerChoices):
+        LOW = 0, "Low"
+        NORMAL = 1, "Normal"
+        HIGH = 2, "High"
+
+    name = models.CharField(max_length=100)
+    desc = models.TextField(max_length=256)
+    is_completed = models.BooleanField(default=False)
+    priority = models.IntegerField(
+        default=TaskPriority.NORMAL, choices=TaskPriority.choices
+    )
+
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="task"
+    )
+
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = "task"
+        verbose_name = "task"
+
+    def __str__(self):
+        return self.name
